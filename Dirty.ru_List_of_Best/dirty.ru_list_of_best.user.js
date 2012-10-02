d71f3ae9590eae6a4dc6f2a6d5ed27a4209f8e25
Infocatcher <infocatcher.dev.null@gmail.com>
2012-10-02 14:07:49 +0400

    Highlight selected post and don't change location.hash

--- Dirty.ru_List_of_Best/dirty.ru_list_of_best.user.js
+++ Dirty.ru_List_of_Best/dirty.ru_list_of_best.user.js
@@ -2,7 +2,7 @@
 // @name        Dirty.ru List of Best
 // @namespace   dev/null
 // @include     http://dirty.ru/comments/*
-// @version     0.1.2
+// @version     0.1.3pre
 // ==/UserScript==
 
 (function() {
@@ -50,39 +50,81 @@ Array.prototype.forEach.call(
 var div = document.createElement("div");
 div.id = "__userJs__bestList";
 div.innerHTML = '\
-<style type="text/css">\
-	#__userJs__bestList {\
-		position: fixed !important;\
-		top: 2px !important;\
-		right: 3px !important;\
-		z-index: 2147483647 !important;\
-		color: #999 !important;\
-	}\
-	#__userJs__bestList, #__userJs__bestList ul, #__userJs__bestList li {\
-		list-style-type: none !important;\
-		margin: 0 !important;\
-		padding: 0 !important;\
-		text-align: right !important;\
-	}\
-	#__userJs__bestList li {\
-		margin-right: 2px !important;\
-	}\
-	#__userJs__bestList ul {\
-		line-height: 1.3em !important;\
-		max-height: 32.5em !important; /* 1.3*25 */\
-		min-width: 5em !important;\
-		overflow: auto !important;\
-	}\
-	#__userJs__bestList :visited { color: #999 !important; }\
-	#__userJs__bestListClose {\
-		text-decoration: none !important;\
-		margin: 0 1em !important;\
-		padding: 0 0.3em !important;\
-	}\
-	#__userJs__bestListClose:hover { color: #900 !important; }\
-</style>\
-<ul>\n' + (best.join("\n") || 'Не найдено!') + '\n</ul>\
-<a id="__userJs__bestListClose" href="javascript:void 0;" title="Закрыть"\
-	onclick="this.parentNode.parentNode.removeChild(this.parentNode);">x</a>';
+<style type="text/css">\n\
+	.comment:target > .comment_inner { outline: 1px solid #c4cedb !important; }\n\
+	.comment:target > .comment_inner:hover { outline: 1px solid #afbecf !important; }\n\
+	.comment.__userJs__bestListTarget > .comment_inner { outline: 1px solid #e0e0e0 !important; }\n\
+	.comment.__userJs__bestListTarget > .comment_inner:hover { outline: 1px solid #d0d0d0 !important; }\n\
+	#__userJs__bestList {\n\
+		position: fixed !important;\n\
+		top: 2px !important;\n\
+		right: 3px !important;\n\
+		z-index: 2147483647 !important;\n\
+		color: #999 !important;\n\
+	}\n\
+	#__userJs__bestList, #__userJs__bestList ul, #__userJs__bestList li {\n\
+		list-style-type: none !important;\n\
+		margin: 0 !important;\n\
+		padding: 0 !important;\n\
+		text-align: right !important;\n\
+	}\n\
+	#__userJs__bestList li {\n\
+		margin-right: 2px !important;\n\
+	}\n\
+	#__userJs__bestList ul {\n\
+		line-height: 1.3em !important;\n\
+		max-height: 32.5em !important; /* 1.3*25 */\n\
+		min-width: 5em !important;\n\
+		overflow: auto !important;\n\
+	}\n\
+	#__userJs__bestList :visited { color: #999 !important; }\n\
+	#__userJs__bestListClose {\n\
+		text-decoration: none !important;\n\
+		margin: 0 0.3em !important;\n\
+		padding: 0 0.3em !important;\n\
+	}\n\
+	#__userJs__bestListClose:hover { color: #900 !important; }\n\
+</style>\n\
+<ul>\n' + (best.join("\n") || 'Не найдено!') + '\n</ul>\n\
+<a id="__userJs__bestListClose" href="javascript://close" title="Закрыть"\>x</a>';
 document.body.appendChild(div);
+
+div.addEventListener("click", clickHandler, true);
+window.addEventListener("unload", destroy, false);
+function clickHandler(e) {
+	if(e.button != 0)
+		return;
+	var a = getLink(e.target);
+	if(a.id == "__userJs__bestListClose") {
+		var block = a.parentNode;
+		block.parentNode.removeChild(block);
+		stopEvent(e);
+	}
+	else if(/^#(\d+)$/.test(a.getAttribute("href"))) {
+		var id = RegExp.$1;
+		var anch = document.getElementById(id) || document.getElementsByName(id)[0];
+		if(anch && "scrollIntoView" in anch) {
+			anch.scrollIntoView();
+			var trgClass = "__userJs__bestListTarget";
+			var trg = document.getElementsByClassName(trgClass);
+			trg.length && trg[0].classList.remove(trgClass);
+			anch.classList.add(trgClass);
+			stopEvent(e);
+		}
+	}
+}
+function getLink(node) {
+	for(; node; node = node.parentNode)
+		if(node.nodeName.toLowerCase() == "a")
+			return node;
+	return null;
+}
+function stopEvent(e) {
+	e.preventDefault();
+	e.stopPropagation();
+}
+function destroy() {
+	div.removeEventListener("click", clickHandler, true);
+	window.removeEventListener("unload", destroy, false);
+}
 })();
