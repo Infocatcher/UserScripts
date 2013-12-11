11607f85ac0ca7b9f603e1e3329e35fc8ce68fbb
Infocatcher <infocatcher.dev.null@gmail.com>
2013-12-11 13:24:01 +0400

    Bump version to 0.2.0pre7 - 2013-12-11

--- Remove_Fake_Links/remove_fake_links.user.js
+++ Remove_Fake_Links/remove_fake_links.user.js
@@ -2,7 +2,7 @@
 // @name        Remove fake links
 // @description Remove tracking redirects like http://www.google.com/url?... and http://clck.yandex.ru/redir/...
 // @author      Infocatcher
-// @version     0.2.0pre6 - 2013-09-22
+// @version     0.2.0pre7 - 2013-12-11
 // @run-at      document-start
 // @namespace   dev/null
 // @include     http://www.google.*/search?*
