ececd0085a1e8c35e6ac8170b9197d97ed45259d
Infocatcher <infocatcher.dev.null@gmail.com>
2022-06-20 00:03:03 +0300

    Basic indication: set and change window title prefix during loading

--- VseInstrumenti.ru_Load_all_favorites/VseInstrumenti.ru_Load_all_favorites.user.js
+++ VseInstrumenti.ru_Load_all_favorites/VseInstrumenti.ru_Load_all_favorites.user.js
@@ -1,5 +1,6 @@
 ﻿// ==UserScript==
 // @name        VseInstrumenti.ru: Load all favorites
+// @author      Infocatcher
 // @namespace   dev/null
 // @include     https://www.vseinstrumenti.ru/user/favorites
 // @version     0.1.0pre
@@ -7,6 +8,7 @@
 // ==/UserScript==
 
 (function iteration() {
+	var ttls = ["⏳ ", "⌛️ "];
 	var msg = "[VSI loader]: ";
 	var btnNext = iteration.__btnNext || (iteration.__btnNext = (function() {
 		var btns = document.getElementsByTagName("button");
@@ -33,7 +35,23 @@
 		setTimeout(iteration, 200);
 		return;
 	}
+
+	var indx = "__index" in iteration ? ++iteration.__index : (iteration.__index = 0);
+	if(indx >= ttls.length)
+		indx = iteration.__index = 0;
+	document.title = ttls[indx] + unprefix();
+
 	console.log(msg + "load next");
 	btnNext.click();
 	setTimeout(iteration, 300);
+
+	function unprefix() {
+		var t = document.title;
+		for(var i = 0, l = ttls.length; i < l; ++i) {
+			var p = ttls[i];
+			if(t.startsWith(p))
+				return t.substr(p.length);
+		}
+		return t;
+	}
 })();
