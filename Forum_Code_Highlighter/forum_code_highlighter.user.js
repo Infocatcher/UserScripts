d059ad3754fa4208322bde90bb80c1a1a1bfb9c7
Infocatcher <infocatcher.dev.null@gmail.com>
2023-10-28 19:27:09 +0300

    Bump version to 0.2.0b2 - 2023-10-28

--- Forum_Code_Highlighter/forum_code_highlighter.user.js
+++ Forum_Code_Highlighter/forum_code_highlighter.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name        Forum Code Highlighter
-// @version     0.2.0b1 - 2023-09-30
+// @version     0.2.0b2 - 2023-10-28
 // @author      Infocatcher
 // @namespace   dev/null
 // @match       *://custombuttons.sourceforge.net/forum/viewtopic.php?*
